f30077b28b994451ff8a6ca122c880bfc6481a9f
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2024-07-24 17:44:22 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,1 +1,33 @@
-s
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const authRoutes = require('./routes/auth');
+const taskRoutes = require('./routes/tasks');
+const { connection } = require('./config/db');
+const PORT = process.env.PORT || 8001
+const app = express();
+// mongoose.connect('mongodb://localhost:27017/task-manager', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
+
+// app.use((err, req, res, next) => {
+//     res.status(500).json({ error: err.message });
+// });
+
+app.listen(PORT, async ()=> {    
+    try {
+        await connection;
+        console.log("connected to DB")
+    } catch (err) {
+        console.log("not connected to db")    
+        console.log(err)
+    }
+    console.log(`listning on PORT ${PORT}`)
+})
+
+module.exports = app;
